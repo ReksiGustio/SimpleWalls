f1898c47b36f86ed81abcd47a2d09cace42c387d
Reksi Andika Gustio <reksiandika.business@gmail.com>
2024-07-02 19:08:13 +0700

    Integrating picture upload for post and comment

---
+++ SimpleWalls/View/Profile/PostPictureView.swift
@@ -0,0 +1,18 @@
+//
+//  PostPictureView.swift
+//  SimpleWalls
+//
+//  Created by Reksi Gustio on 02/07/24.
+//
+
+import SwiftUI
+
+struct PostPictureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostPictureView()
+}
